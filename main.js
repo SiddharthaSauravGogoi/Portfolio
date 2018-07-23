4db437ceff4a556e4a835e24acc17c697a8d9d11
SiddharthaSauravGogoi <lezaroth3@gmail.com>
2018-07-23 21:15:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+new TypeIt('#example2', {
+    strings: ["This is a great string.", "But here is a better one.", "And another one"],
+    speed: 50,
+    breakLines: false,
+    autoStart: false
+});
